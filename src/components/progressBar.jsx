22b732e19b66db7539fefabc83d0aab50033ac4e
naolbm <profesornaoltena@gmail.com>
2024-06-16 19:56:54 +0300

    custom player progress bar added v4

--- src/components/progressBar.jsx
+++ src/components/progressBar.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 
-// export default function ProgressBar({ progress }) {
-//   console.log("progress >>> >>", progress);
-
-//   return (
-//     <div
-//       className={`transition-opacity ease-in duration-700 ${
-//         progress === 1 ? "opacity-0" : "opacity-100"
-//       } absolute top-[10px] px-2 py-1 flex justify-center items-center left-[10px] bg-white/20`}
-//     >
-//       <div className={`text-white`}>{progress * 100}%</div>
-//     </div>
-//   );
-// }
-
 export default function ProgressBar({ progress }) {
-  console.log("progress >>> >>", (progress * 100).toFixed(0));
+  console.log("progress >>> >>", progress);
 
   return (
     <div
       className={`transition-opacity ease-in duration-700 ${
         progress === 1 ? "opacity-0" : "opacity-100"
-      } absolute top-0 w-full h-[4px] left-0 bg-white/20`}
+      } absolute top-[10px] px-2 py-1 flex justify-center items-center left-[10px] bg-white/20`}
     >
-      <div
-        className={`bg-white w-[${(progress * 100).toFixed(0)}%] h-full`}
-      ></div>
+      <div className={`text-white`}>{progress * 100}%</div>
     </div>
   );
 }
+
+// export default function ProgressBar({ progress }) {
+//   console.log("progress >>> >>", (progress * 100).toFixed(0));
+
+//   return (
+//     <div
+//       className={`transition-opacity ease-in duration-700 ${
+//         progress === 1 ? "opacity-0" : "opacity-100"
+//       } absolute top-0 w-full h-[4px] left-0 bg-white/20`}
+//     >
+//       <div
+//         className={`bg-white w-[${(progress * 100).toFixed(0)}%] h-full`}
+//       ></div>
+//     </div>
+//   );
+// }
